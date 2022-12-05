794194a8a810818f841068f9e3b82dba21d695b6
Callum Flack <callum@vana.org>
2022-12-05 11:03:16 +1000

    Update 05.js

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -9,21 +9,31 @@ import '../box-styles.css'
 // 💰 Here are the available class names: box, box--large, box--medium, box--small
 // 💰 each of the elements should have the "box" className applied
 
+const Box = ({className = '', style, size, ...otherProps}) => (
+  <div 
+    className={`box box--${size} ${className}`} 
+    style={{fontStyle: 'italic', ...style}} 
+    {...otherProps} 
+  />
+)
+
 // 🐨 add a style prop to each div so their background color
 // matches what the text says it should be
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
-
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
